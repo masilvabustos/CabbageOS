8ac1cf81b116e00d658b7361bf45846ff00cbf28
Matías Silva Bustos <matiasasb@gmail.com>
2015-05-13 23:30:11 -0300

    renamed:    ../init/_kstart.c -> ../init/kernel_init.c 	new file:   cortex-m3/cisr.c

---
+++ kernel/sched/cortex-m3/cisr.c
@@ -0,0 +1,43 @@
+
+void Reset(void)
+{
+	kernel_init();
+}
+
+void Default_Handler(void)
+{
+	while(1);
+}
+
+#define _handler __attribute__ ((weak, alias("Default_Handler")))
+
+void _handler NMI_Handler()
+{
+	while (1);
+}
+
+void _handler HardFault_Handler()
+{
+	while (1);
+}
+
+void _handler MemManage_Handler();
+void _handler BusFault_Handler()
+{
+	while(1);
+}
+
+void _handler UsageFault_Handler()
+{
+	while(1);
+}
+
+void _handler SVC_Handler();
+void _handler PendSV_Handler();
+void _handler DebugMon_Handler()
+{
+	while  (1);
+}
+
+void _handler SysTick_Handler();
+
