8def6e6398c32b96088fe34c49d24957bdeaff73
Matías Silva Bustos <matiasasb@gmail.com>
2015-05-13 23:31:01 -0300

    modified:   kernel_init.c 	modified:   ../sched/Build.gmake

--- kernel/init/kernel_init.c
+++ kernel/init/kernel_init.c
@@ -1,9 +1,17 @@
 
+#include <stdint.h>
+
+#include <stm32f100xx.h>
+
+void kernel_init(void);
+
+
 void kernel_init()
 {
 	extern uint8_t __init_data_start, __init_data_end, __data_start, __bss_start, __bss_end;
+	uint32_t cr, mask, set_mask, reset_mask, conf;
 
-	uint8_t *src, dst;
+	uint8_t *src, *dst;
 
 	src = &__init_data_start;
 	dst = &__data_start;
@@ -12,6 +20,14 @@ void kernel_init()
 
 	for (dst = &__bss_start; dst != &__bss_end; ++dst)
 		*dst = 0;
+
+	RCC_APB2ENR |= 0x10;
+
+	GPIOC_CRH = 0x44444422;
+
+	GPIOC_ODR = 0xffffffff;
+
+	while(1);
 
 }
 
