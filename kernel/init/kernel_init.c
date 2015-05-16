cfadd71761beb137e2d576acbec86959f33243c0
Matías Silva Bustos <masilvabustos@ieee.org>
2015-05-16 13:24:52 -0300

    Provisionally kernel_init calls main().

--- kernel/init/kernel_init.c
+++ kernel/init/kernel_init.c
@@ -1,15 +1,13 @@
 
 #include <stdint.h>
 
-#include <stm32f100xx.h>
-
 void kernel_init(void);
-
 
 void kernel_init()
 {
 	extern uint8_t __init_data_start, __init_data_end, __data_start, __bss_start, __bss_end;
 	uint32_t cr, mask, set_mask, reset_mask, conf;
+	register void* sp asm ("sp");
 
 	uint8_t *src, *dst;
 
@@ -21,11 +19,7 @@ void kernel_init()
 	for (dst = &__bss_start; dst != &__bss_end; ++dst)
 		*dst = 0;
 
-	RCC_APB2ENR |= 0x10;
-
-	GPIOC_CRH = 0x44444422;
-
-	GPIOC_ODR = 0xffffffff;
+	main();
 
 	while(1);
 
