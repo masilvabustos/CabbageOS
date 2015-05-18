5c65f765f544201eed5969bea4c5c48fee6df9e0
Matías Silva Bustos <masilvabustos@ieee.org>
2015-05-18 14:44:26 -0300

    Disabled some code.

--- include/ucontext.h
+++ include/ucontext.h
@@ -8,6 +8,8 @@
 
 union mcontext {
 	struct mc_s {
+
+		int* 	sp;
 		int 	r4;
 		int 	r5;
 		int 	r6;
@@ -17,15 +19,13 @@ union mcontext {
 		int 	r10;
 		int 	r11;
 
-		void* 	sp;
-
 		int 	r0;
 		int 	r1;
 		int 	r2;
 		int 	r3;
 		int 	r12;
-		void* 	lr;
-		void* 	pc;
+		short* 	lr;
+		short* 	pc;
 		int 	xpsr;
 	} mc_s;
 	int mc_v[sizeof(struct mc_s)/sizeof(int)];
@@ -33,54 +33,58 @@ union mcontext {
 
 #define MC_OFF(r) offsetof(union mcontext, mc_s.r)
 
-typedef union mcontext mcontext_t;
+typedef union mcontext* mcontext_t;
 
 #define __unused __attribute__ ((unused))
 
-#define __ldr(r,b,off) asm volatile ("ldr %0,[%1,%2]" : : "r" (r), "r"(b), "i" (off))
-#define __str(r,b,off) asm volatile ("str " #r ",[%0,%1]" : : "r"(b), "i" (off)) 
-#define __ldm(m,b,r) asm volatile ("ldm" #m " " #b "," #r); 
-#define __stm(m,b,r) asm volatile ("stm" #m " " #b "," #r);
-#define __push(r) asm volatile ("push " #r)
+#define __ldr_i(r,b,off) asm volatile ("ldr %0,[%1,%2]" : "=r" (r) : "r"(b), "i" (off))
+#define __str_i(r,b,off) asm volatile ("str %0,[%1,%2]" : : "r"(r), "r"(b), "i" (off)) 
+#define __str_post(r,b,off) asm volatile ("str %1,[%0],%2" : "+r" (b) : "r" (r), "i"(off))
+#define __ldm(m,b,rlst) asm volatile ("ldm" #m " %0," rlst :: "r"(b)); 
+#define __ldm_wb(m,b,rlst) asm volatile ("ldm" #m " %0," rlst : "+r"(b)); 
+#define __stm(m,b,rlst) asm volatile ("stm" #m " %0," rlst :: "r"(b))
+#define __stm_wb(m,b,rlst) asm volatile ("stm" #m " %0!," rlst : "+r" (b))
+
+#define __push(r) asm volatile ("push " r)
+#define __pop(r) asm volatile ("pop " r)
 #define __restore(r,b) __ldr(r,b,MC_OFF(r))
 #define __save(r,b) __str(#r,b,offsetof(struct ucontext,uc_mcontext.mc_s.r))
-#define __msr(s,r) asm volatile ("msr " #s "," #r)
-#define __mrs(r,s) asm volatile ("mrs " #r "," #s)
+#define __msr(s,r) asm volatile ("msr " #s ",%0": : "r"(r))
+#define __mrs(r,s) asm volatile ("mrs %0," #s : "=r" (r))
 
 /* For proper use of this function, the structure which mcp points to must be at top 
  * of the restored context stack.
  * */
-
-static  __attribute__((naked,noreturn)) void __restore_mcontext(const union mcontext*__restrict__ mc) 
-{
-	register void* r0 asm ("r0");
-	register int r1 asm ("r1");
-	register int sp asm ("sp");
-	register union mcontext* r12 asm ("r12");
-	r12 = r0;
 #if 0
-	__restore(r3,r1);
-	__restore(r4,r1);
-	__restore(r5,r1);
-	__restore(r6,r1);
-	__restore(r7,r1);
-#endif
+static  __attribute__((naked,noreturn)) void __restore_mcontext() 
+{
+	register int r1 asm ("r1");
+	register int r12 asm ("r12");
+	register int* r0 asm ("r0");
+	register int lr asm ("lr");
+	register int* sp asm ("sp");
 
-	__ldr(r1,r12,MC_OFF(xpsr));
-	__msr(apsr_nzcvq,r1);
-	__ldr(r1,r12,MC_OFF(pc));
+	asm volatile ("mov r12,r0");
 
-	__ldm(ia,r12!,{r4-r11});
-	asm volatile ("ldr r2,[r12],#4");
-	asm volatile ("mov sp,r2");
-	__push({r1});
-	__ldm(ia,r12!,{r0-r3});
-	asm volatile ("ldr r12,[r12]");
+	__ldr_i(r0,r12,MC_OFF(xpsr));	
+	__msr(apsr_nzcvq,r0);
+
+	__ldr_i(r0,r12,MC_OFF(r12));
+	__ldr_i(r1,r12,MC_OFF(pc));
+	__push("{r0,r1}");
+	
+	__ldr_i(sp,r12,0);
+	asm volatile ("add r12,r12,4");
+	__ldm_wb(ia,r12,"{r4-r11}");
+	__ldm_wb(ia,r12,"{r0-r3}");
+	__ldr_i(lr,r12,4);
 
 	/* For some oscure reason we shall know the size of this function. ;) */
-	asm volatile (	"__restore_mcontext_end__:\n	"
-		      	"	pop {pc}		");
+	asm volatile (	"__restore_mcontext_end__:\n"
+		      	"	pop {r12,pc}");
 }
+	
+#endif
 
 struct ucontext {
 	
@@ -93,47 +97,21 @@ struct ucontext {
 
 typedef struct ucontext ucontext_t;
 
-#if 1
-__attribute__ ((naked)) void __save_mcontext(union mcontext *__restrict__ mcp) 
-{
-	register void* r0 asm ("r0");
-	register int r1 asm ("r1");
-	register int sp asm ("sp");
-	register int lr asm ("lr");
-
-	r0 = mcp;
 #if 0
-	__str(sp,r0,MC_OFF(sp));
-#endif
-	__mrs(r1,apsr);
-	__str(r1,r0,MC_OFF(xpsr));
-	__str(lr,r0,MC_OFF(pc));
-
-	__stm(ia,r0!,{r4-r11});
-#if 1
-	asm volatile ("str sp,[r0],#8"); // jump r0
-#else
-	__stm(ia,r0!,{sp});
-#endif
-	asm volatile ("stmia r0!,{r1-r3,r12}");
-	
-	asm volatile ("bx lr");	
-}
-#endif
-
 int getcontext(ucontext_t *ucp)
 {
-	__save_mcontext(&ucp->uc_mcontext);
+	ucp->uc_stack.ss_sp 	= __uc_current_stack.ss_sp;
+	ucp->uc_stack.ss_size 	= __uc_current_stack.ss_size;
+	__ucp->
 
-	return 0;
-}
 
 int setcontext(ucontext_t *ucp)
 {
 	__restore_mcontext(&ucp->uc_mcontext);
 	return -1;
 }
-
+#endif
+#if 0
 int __attribute__ ((naked)) swapcontext(ucontext_t *__restrict__ oucp, const ucontext_t *__restrict__ ucp)
 {
 	register void* lr asm ("lr");
@@ -167,7 +145,7 @@ void makecontext(ucontext_t* ucp, void(*func)(), int argc, ...)
 	ucp->uc_mcontext.mc_s.sp = sp;
 }
 
-	
+#endif	
 			
 	
 			
