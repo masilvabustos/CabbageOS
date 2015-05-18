b7b04646d02bffcf7a46cdf11a1b0e9f13e39113
Matías Silva Bustos <masilvabustos@ieee.org>
2015-05-18 02:15:17 -0300

    New files.
    	new file:   stm32f100rb.h
    	modified:   stm32f100xx.ld
    	new file:   stm32vldiscovery.h
    	new file:   stm32vldiscovery.ld

---
+++ hw/stm32f100rb.h
@@ -0,0 +1,9 @@
+
+#ifndef __stm32f100rb_h_INCLUDED__
+#define __stm32f100rb_h_INCLUDED__
+
+#define STM32F100XX_LD
+
+#include "stm32f100xx.h"
+
+#endif
---
+++ hw/stm32vldiscovery.h
@@ -0,0 +1,7 @@
+
+#ifndef __stm32vldiscovery_h_INCLUDED__
+#define __stm32vldiscovery_h_INCLUDED__
+
+#include "stm32f100rb.h"
+
+#endif
