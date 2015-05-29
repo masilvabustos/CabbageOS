c374e26a3c7f049910e8403e6ac95d65e4df5529
Matías Silva Bustos <masilvabustos@ieee.org>
2015-05-29 11:29:09 -0300

    wip

--- arch/cortex-m3/kernel/sched/sched.c
+++ arch/cortex-m3/kernel/sched/sched.c
@@ -1,20 +1,70 @@
 
 #include <task.h>
+#include <stdint.h>
 
-struct cabb_task* parent_task;
-struct cabb_task* current_task;
-enum sched_op {
-	DISPATCH
-	PREEMPT
-} switchop;
+struct signal_vectors {
+	uint32_t 	 sv_onstackv;
+	uint32_t	 sv_siginfov;
+	uint32_t 	 sv_maskv[32];
+	stack_t		 sv_stack;
+	union {
+		void   (*sv_sigaction)(int,struct siginfo*,void*);
+		void   (*sv_handler)(int);
+	};
+};
 
-void sched_switch(int* sp)
+struct task_state {
+	clock_t		ts_slice;
+	int		ts_type;
+	uint8_t		ts_data[];
+};
+
+struct task_context;
+
+typedef int	mcontext_t[16];
+
+static volatile mcontext_t *current_link;
+//struct task_context	*task_link;
+struct task_state	*task_state;
+
+
+struct task_context {
+	struct task_context	*tc_link;
+	struct task_state	*tc_state;
+	ucontext_t		 tc_ucontext;
+};	
+
+
+extern __swapmcontext(mcontext_t *, mcontext_t *);
+
+void do_task_switch()
 {
+	mcontext_t mcontext, *saved_link;
+	
+
+	switch(switch_action){
+
+	case TASK_SWITCH_ACTION_PREEMPTION:
+		
+		break;
+
+	case TASK_SWITCH_ACTION_DISPATCH:
+		saved_link = current_link;
+		__swapmcontext(&mcontext, &task->ts_mcontext);
+		current_link = saved_link;
+		
+	case TASK_SWITCH_ACTION_YIELD:
+		__swapmcontext(&current_task->ts_mcontext, current_link);
+	}
+}
+
+void restore_context()
+{
+	
 	struct cabb_task *task;
 
 	task = sp + (offsetof(struct cabb_task, t_ucontext.uc_mcontext.mc_s.r4));
 
-	mother = task;
 	
 	sp = ready->t_ucontext.uc_stack.ss_sp;
 	for(p =  )
