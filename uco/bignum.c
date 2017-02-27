53c27ce05371069cdbaa4011e3dd7c9d519afe27
Matías Silva Bustos <matiasasb@gmail.com>
2017-02-26 22:26:19 -0300

    Add multiplication and division operations.

--- uco/bignum.c
+++ uco/bignum.c
@@ -1,41 +1,89 @@
 
 #include <stdint.h>
 
-struct uint296 {
-	uint64_t a1[4];
-	uint32_t a2;
-	uint8_t a3;
-};
+#define __PACKED__ __attribute__ ((packed))
 
-struct uint296 uint296_div8(struct uint296 x, uint8_t d)
+typedef unsigned __int128 uint128_t;
+
+struct u296 {
+	uint64_t b0_255[4];
+	unsigned long int b256_295 : 40;
+} __PACKED__;
+
+uint8_t __div_u296_u8(struct u296 *x, uint8_t d)
 {
-	struct uint296 q;
-	uint64_t t64;
-	unsigned __int128 t128;
+        uint128_t r;
+	int i;
 	
-	q.a3 = x.a3 / d;
+	r = x->b256_295;
 
-	t64 = x.a3 % d;
-	t64 <<= 32;
-	t64 &= x.a2;
+	x->b256_295 = r / d;
 
-	q.a2 = t64 / d;
+	for (i=3; i>= 0; --i) {
+		r %= d;
+		r <<= 64;
+		r |= x->b0_255[i];
+		
+		x->b0_255[i] = r/d;
+	}
 
-	t128 = t64 % d;
-	t128 <<= 64;
-	t128 &= x.a1[3];
+	r %= d;
 
-	q.a1[3] = t128 / d;
+        return r;
+}
 
-	t128 = ((t128 % d) << 64) & x.a1[2];
 
-	q.a1[2] = t128 / d;
+void __mul_u296_u8(struct u296 *x, uint8_t f)
+{
+        uint128_t p;
+	int i;
+	
+	p = 0;
 
-	t128 = ((t128 % d) << 64) & x.a1[1];
-	q.a1[1] = t128 / d;
+	for (i=0; i<4; ++i) {
+		p += (uint128_t) x->b0_255[i] * f;
+		x->b0_255[i] = p;
+		p >>= 64;
+        };
 
-	t128 = ((t128 % d) << 64) & x.a1[0];
-	q.a1[0] = t128 / d;
+	p += x->b256_295 * f;
+	x -> b256_295 = p;
 
-	return q;
+}
+
+
+#include <stdio.h>
+
+void printu296(struct u296 x)
+{
+	printf("%02hhx:%08x:%016lx:%016lx:%016lx:%016lx\n", (uint8_t) (x.b256_295 >> 32), (uint32_t) x.b256_295, x.b0_255[3], x.b0_255[2], x.b0_255[1], x.b0_255[0] );
+}
+
+int main()
+{
+	struct u296 x;
+	unsigned __int128 p;
+	unsigned int f = 253;
+	
+	printf("sizeof(u296) = %lu\n", sizeof(struct u296)*8);
+
+	x = (struct u296) {{0xabcd111212211, 0xaaabbbcccaa3322, 0x3342351ddcc42344, 0x232adad1a3232323}, 0xabcd};
+
+	printu296(x);
+	p = (uint128_t) x.b0_255[0];
+	
+	__mul_u296_u8(&x, f);
+	printu296(x);
+	
+	
+	//	p *= f;
+	//printf("esperado: %016lx:%016lx\n", (uint64_t) (p >> 64), (uint64_t) p); 
+
+
+
+	__div_u296_u8(&x, f);
+
+	printu296(x);
+	
+	return 0;
 }
